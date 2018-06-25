0bf5674c275fec446a5ae5be5115b0bd4dd2dac3
rajdeepsharma17 <rajdeep.sharma17@gmail.com>
2018-06-25 23:44:25 +0530

    added ClarityUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http'
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -9,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [AppComponent],
   bootstrap: [AppComponent]
