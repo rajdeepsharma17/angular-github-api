702eae9fefc32715ffae6c4b30bb8c5c21e714ea
rajdeepsharma17 <rajdeep.sharma17@gmail.com>
2018-06-25 22:23:25 +0530

    set up HttpRequest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http'
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  user = 'rajdeepsharma17'
+  repo = 'SIH_Kerela'
+
+  private apiUrl = 'https://api.github.com/repos/' + this.user + '/' + this.repo
+  data: any = {}
+
+  constructor(private http:Http){
+    this.getContacts()
+    this.getData()
+  }
+
+  getData(){
+    return this.http.get(this.apiUrl).pipe(
+      map((res: Response) => res.json())
+    )
+  }
+
+  getContacts(){
+    this.getData().subscribe(data => {
+      console.log(data)
+      this.data = data
+      console.log(data.archive_url)
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
